df36674f25172b7e6ce231d3d29a03860ec5c61f
Andrew MacVey <macvey@usc.edu>
2019-06-24 22:51:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo (string){
-  return 'Happy holidays, ${string}!'
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
+
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
